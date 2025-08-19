900ed24aa7467b3823e74c72d8481176d90d5ce2
pyaesone251 <pyaesonepaing06@gmail.com>
2025-08-20 02:09:24 +0630

    blog

--- myapp/models.py
+++ myapp/models.py
@@ -28,5 +28,52 @@ class ServiceworkModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updeated_at = models.DateTimeField(auto_now=True)
 
+class ResumeEducation(models.Model):
+    maintitle = RichTextField(null=True,blank=True)
+    subtitle = RichTextField(null=True,blank=True)
+    description = RichTextField(null=True,blank=True)
+    experience = RichTextField(null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updeated_at = models.DateTimeField(auto_now=True)
+
+class EducationExperience(models.Model):
+    title = RichTextField(null=True,blank=True)
+    year = RichTextField(null=True,blank=True)
+    description = RichTextField(null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updeated_at = models.DateTimeField(auto_now=True)
+
+class PortfolioAera(models.Model):
+    title = RichTextField(null=True,blank=True)
+    name = RichTextField(null=True,blank=True)
+    description = RichTextField(null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updeated_at = models.DateTimeField(auto_now=True)
+
+class Portfolio(models.Model):
+    image = models.ImageField(upload_to='image',null=True,blank=True)
+    title = RichTextField(null=True,blank=True)
+    name = RichTextField(null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updeated_at = models.DateTimeField(auto_now=True)
+
+class BlogAera(models.Model):
+    title = RichTextField(null=True,blank=True)
+    name = RichTextField(null=True,blank=True)
+    description = RichTextField(null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updeated_at = models.DateTimeField(auto_now=True)
+
+class Blog(models.Model):
+    image1 = models.ImageField(upload_to='blog',null=True,blank=True)
+    image2 = models.ImageField(upload_to='blog',null=True,blank=True)
+    date = RichTextField(null=True,blank=True)
+    people = RichTextField(null=True,blank=True)
+    comment = RichTextField(null=True,blank=True)
+    title = RichTextField(null=True,blank=True)
+    note = RichTextField(null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updeated_at = models.DateTimeField(auto_now=True)
+    
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -7,10 +7,21 @@ def Indexview(request):
     position = PositionModel.objects.all()
     about = AboutModel.objects.all()
     work = ServiceworkModel.objects.all().order_by('id')[:4]
-
+    education = ResumeEducation.objects.all()
+    experience = EducationExperience.objects.all().order_by('id')[:4]
+    aera = PortfolioAera.objects.all()
+    porofolio = Portfolio.objects.all().order_by('id')[:6]
+    blogaera = BlogAera.objects.all()
+    blog = Blog.objects.all().order_by('id')[:4]
     context = {
         'position':position,
         'about':about,
         'work':work,
+        'education':education,
+        'experience':experience,
+        'aera':aera,
+        'porofolio':porofolio,
+        'blogaera':blogaera,
+        'blog':blog,
     }
     return render(request, 'index.html', context)
